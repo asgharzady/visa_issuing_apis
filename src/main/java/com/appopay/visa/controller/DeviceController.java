a1096fecf2362ac5f2d6b3b9d610b3d9adcdfc19
asgharzady <asgharzady@gmail.com>
2024-10-08 10:04:14 +0500

    added logs

--- src/main/java/com/appopay/visa/controller/DeviceController.java
+++ src/main/java/com/appopay/visa/controller/DeviceController.java
@@ -34,41 +34,46 @@ public class DeviceController {
     public ResponseEntity<DeviceBindResponseDTO> bindDevice(@RequestBody DeviceEnquiryRequestDTO request) {
         log.info("binding device of: " + request.toString());
         String previousDeviceId = deviceService.bindDevice(request.getDeviceId(), request.getMobileNo(), null);
+        log.info("binding done of: " + request.getDeviceId());
         return ResponseEntity.ok().body(new DeviceBindResponseDTO(previousDeviceId));
     }
 
     @PostMapping(value = "reBind")
     public ResponseEntity<DeviceBindResponseDTO> reBindDevice(@RequestBody DeviceEnquiryRequestDTO request) {
-        log.info("re binding of: " + request.toString());
+        log.info("re binding request of: " + request.toString());
         String previousDeviceId = deviceService.reBindDevice(request.getDeviceId(), request.getMobileNo());
+        log.info("re binding done of: " + request.getDeviceId());
         return ResponseEntity.ok().body(new DeviceBindResponseDTO(previousDeviceId));
     }
 
     @PostMapping("/save-pin")
     public ResponseEntity<ResponseDTO> savePin(@Valid @RequestBody DevicePinSaveRequestDTO request) {
-        log.info("saving pin of: " + request.toString());
+        log.info("saving pin of: " + request.getDeviceId());
         deviceService.savePin(request.getDeviceId(), request.getMobilePin());
+        log.info("Mobile PIN saved successfully: " + request.getDeviceId());
         return ResponseEntity.ok(new ResponseDTO("Mobile PIN saved successfully"));
     }
 
     @PostMapping("/update-pin")
     public ResponseEntity<ResponseDTO> updatePin(@Valid @RequestBody DevicePinSaveRequestDTO request) {
-        log.info("updating pin of: " + request.toString());
+        log.info("updating pin of: " + request.getDeviceId());
         deviceService.savePin(request.getDeviceId(), request.getMobilePin());
+        log.info("Mobile PIN updated successfully: " + request.getDeviceId());
         return ResponseEntity.ok(new ResponseDTO("Mobile PIN updated successfully"));
     }
 
     @PostMapping("/change-pin")
     public ResponseEntity<ResponseDTO> changePin(@Valid @RequestBody DevicePinSaveRequestDTO request) {
-        log.info("changePin pin of: " + request.toString());
+        log.info("changePin pin of: " + request.getDeviceId());
         deviceService.verifyPin(request.getDeviceId(), request.getOldPin());
         deviceService.savePin(request.getDeviceId(), request.getMobilePin());
+        log.info("Mobile PIN updated successfully: " + request.getDeviceId());
         return ResponseEntity.ok(new ResponseDTO("Mobile PIN updated successfully"));
     }
 
     @PostMapping("/verify-pin")
     public ResponseEntity<VerifyResponseDTO> verifyPin(@Valid @RequestBody DeviceVerifyPinRequestDTO request) {
-        log.info("verifying pin of: " + request.toString());
+        log.info("verifying pin of: " + request.getDeviceId());
         String response = deviceService.verifyPin(request.getDeviceId(), request.getMobilePin());
         log.info("verification response " + response);
         return ResponseEntity.ok(new VerifyResponseDTO(response));
