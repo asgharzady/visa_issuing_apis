94abd367b19512f122d01a377fb1ebe1b14dd1e8
asgharzady <asgharzady@gmail.com>
2024-12-10 00:17:18 +0500

    added profiles

--- src/main/java/com/appopay/visa/configuration/SwaggerConfig.java
+++ src/main/java/com/appopay/visa/configuration/SwaggerConfig.java
@@ -1,23 +1,38 @@
 package com.appopay.visa.configuration;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
+    private final Environment environment;
+
+    public SwaggerConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "BearerAuth";
 
+        if (isProdProfileActive()) {
+            return createProdOpenAPI(securitySchemeName);
+        } else {
+            return createDevOpenAPI(securitySchemeName);
+        }
+    }
+
+    private OpenAPI createProdOpenAPI(String securitySchemeName) {
         return new OpenAPI()
                 .info(new Info()
                         .title("Appopay AML API")
@@ -34,5 +49,25 @@ public class SwaggerConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
+    }
+
+    private OpenAPI createDevOpenAPI(String securitySchemeName) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Appopay AML API")
+                        .version("1.0")
+                        .description("API documentation with JWT authentication"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+
+    private boolean isProdProfileActive() {
+        return List.of(environment.getActiveProfiles()).contains("prod");
     }
 }
